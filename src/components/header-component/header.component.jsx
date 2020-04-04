752e07633b1f0140b1709ed6a1926167c304ae7c
Arnab <arnab.ganguly@settlin.in>
2020-04-05 03:57:27 +0530

    Saved state

--- src/components/header-component/header.component.jsx
+++ src/components/header-component/header.component.jsx
@@ -2,17 +2,18 @@
 import React from "react";
 import { Typography ,Paper} from "@material-ui/core";
 export default function Header(props){
-	let budget = localStorage.getItem('budget');
+
+	let {year,budget,month,totalExpense}= props;
 	return (
 		<Paper elevation={0} style={{width:"98%",height:"100px",marginTop:"20px", background: 'transparent'}}>
 			<Typography variant="h5">
-				March of fucking 2020
+				{month} of fucking {year}
             </Typography>
 			<Typography variant="h5">
 				you fucking planned to spend <span>{budget} </span>this month
             </Typography>
 			<Typography variant="h5">
-				You have already spent <span>{props.totalExpense}</span>
+				You have already spent <span>{totalExpense}</span>
             </Typography>
 	   </Paper>
 	)
--- src/components/listContainer-component/listContainer.component.jsx
+++ src/components/listContainer-component/listContainer.component.jsx
@@ -1,23 +1,24 @@
-import React, { Component } from 'react'
+import React,{useState} from 'react'
 import {
 	Grid,
 	Paper,
 	Typography
 } from "@material-ui/core";
 import EditOutlinedIcon from "@material-ui/icons/EditOutlined";
-export default function list(props) {
-	
-	
+export default function List(props) {
+const [total,setTotal] = useState(0)
+console.log("rec",props.record);
+	return (
 
-		return (
-			<Grid container lg={12} style={{ backgroundColor: "red" ,marginBottom:"30px"}}>
+		<Paper elevation={1}>
+			<Grid container lg={12} style={{ backgroundColor: "red", marginBottom: "30px" }}>
+				
 				<Grid
 					container
 					lg={12}
 					md={12}
 					justify="space-between"
 					style={{
-						backgroundColor: "blue",
 						padding: "5px",
 						color: "#f4f6f8",
 						backgroundImage: "linear-gradient(to right bottom,#263238,#546e7a)"
@@ -29,24 +30,24 @@ export default function list(props) {
 
 					</Grid>
 					<Grid item md={2} lg={2} alignItems="center">
-						<Typography>TOTAL EXPENSE:900</Typography>
+						<Typography>TOTAL EXPENSE: </Typography>
 					</Grid>
 				</Grid>
+
 				<Grid
 					container
 					lg={12}
 					md={12}
 					justify="space-between"
 					style={{
-						backgroundColor: "blue",
 						padding: "5px",
 						backgroundColor: "#cfd8dc",
 						color: "#37474f"
 					}}
 				>
-					
+
 					<Grid item md={2} lg={2}>
-						
+
 						<Typography><span ><EditOutlinedIcon /> </span> tea</Typography>
 
 					</Grid>
@@ -62,7 +63,6 @@ export default function list(props) {
 					md={12}
 					justify="space-between"
 					style={{
-						backgroundColor: "blue",
 						padding: "5px",
 						backgroundColor: "#eceff1 ",
 						color: "#37474f"
@@ -85,7 +85,6 @@ export default function list(props) {
 					md={12}
 					justify="space-between"
 					style={{
-						backgroundColor: "blue",
 						padding: "5px",
 						backgroundColor: "#cfd8dc",
 						color: "#37474f"
@@ -103,12 +102,13 @@ export default function list(props) {
 				</Grid>
 
 			</Grid>
-		)
-	
+		</Paper>
+	)
+
 }
 
 
 /**
- * 
- * 
+ *
+ *
  */
