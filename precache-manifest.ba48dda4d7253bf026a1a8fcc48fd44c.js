75ed0ba30766462c6142774fb1e035629127f12d
Nisha <nishahs08@outlook.in>
2020-04-06 17:55:42 +0530

    Updates

---
+++ precache-manifest.ba48dda4d7253bf026a1a8fcc48fd44c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e28f562d43716bd296a2fbe0e01d5238",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9743b0740017466fca19",
+    "url": "/static/css/main.0e441b79.chunk.css"
+  },
+  {
+    "revision": "89a322e5d25f999c3a1b",
+    "url": "/static/js/2.45f7d9a1.chunk.js"
+  },
+  {
+    "revision": "28164e9e1f7b49ce7422bf240655ba5b",
+    "url": "/static/js/2.45f7d9a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9743b0740017466fca19",
+    "url": "/static/js/main.f6b51c33.chunk.js"
+  },
+  {
+    "revision": "11e6611cbe91c8acc7cd",
+    "url": "/static/js/runtime-main.632aca02.js"
+  }
+]);
